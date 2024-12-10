d4ebb028bae35878e0a65c96be6340cd3488c278
Stewart Patterson <stewart.patterson@catalinamarketing.co.jp>
2024-12-10 17:19:21 +0900

    Modify getSQL to be able to process queries with {common.parameters} seamlessly

--- My R Profile.R
+++ My R Profile.R
@@ -6,12 +6,45 @@
 options(scipen=999)
 
 #===========================================================================
+# Use Python to read common parameters for yellowbrick queries into yaml
+#===========================================================================
+
+library(yaml)
+
+# Run python script in subprocess to fetch common parameters and store in yaml
+system('python "C:\\Users\\stewapatte\\OneDrive - Catalina Marketing Japan K.K\\ドキュメント\\Code Chunks\\RProfile\\get_common_parameters.py"', intern=TRUE)
+
+# Read yaml
+# common_parameters <- yaml.load_file("C:/Users/stewapatte/OneDrive - Catalina Marketing Japan K.K/ドキュメント/Code Chunks/RProfile/common_parameters.yml")
+
+#===========================================================================
+# Substitute common parameters for yellowbrick queries in a string
+#===========================================================================
+
+substitute_common_parameters <- function(this_sql_line, common_parameters) {
+  this_sql_line<-gsub("\\{common.common_criteria\\(\'tp\'\\)\\}", common_parameters$common_criteria_tp, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_chain_exclude\\(\'tp\'\\)\\}", common_parameters$common_chain_exclude_tp, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_exclude_ord_event_key_tbl.*\\}", common_parameters$common_exclude_ord_event_key_tbl, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_event_dist\\(\'dst\' *, *\'pv\'\\)\\}", common_parameters$common_event_dist_dst_pv, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_event_dist\\(\'dst\'\\)\\}", common_parameters$common_event_dist_dst, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_id_filter\\(\'pos\'\\)\\}", common_parameters$common_id_filter_pos, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_id_filter\\(\'dst\'\\)\\}", common_parameters$common_id_filter_dst, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_id_filter\\(\'red\'\\)\\}", common_parameters$common_id_filter_red, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_event_ctrl\\(\'dst\' *, *\'pv\'\\)\\}", common_parameters$common_event_ctrl_dst_pv, this_sql_line)
+  this_sql_line<-gsub("\\{common.common_event_red\\(\'red\' *, *\'pv\'\\)\\}", common_parameters$common_event_red_red_pv, this_sql_line)
+  return(this_sql_line)
+}
+
+#===========================================================================
 # Read .sql file
 #===========================================================================
+
+library(yaml)
 
 getSQL <- function(filepath){
   con = file(filepath, "r")
   sql.string <- ""
+  common_parameters = yaml.load_file("C:/Users/stewapatte/OneDrive - Catalina Marketing Japan K.K/ドキュメント/Code Chunks/RProfile/common_parameters.yml")
   
   while (TRUE){
     line <- readLines(con, n = 1)
@@ -21,6 +54,8 @@ getSQL <- function(filepath){
     }
     
     line <- gsub("\\t", " ", line)
+    
+    line <- substitute_common_parameters(line, common_parameters)
     
     if(grepl("--",line) == TRUE){
       line <- paste(sub("--","/*",line),"*/")
---
+++ get_common_parameters.py
@@ -0,0 +1,23 @@
+import yaml
+import sys
+sys.path.append("I:/SAG_PUBLIC/python/common")
+sys.path.append("//jpnas1a/Shared/SAG_PUBLIC/python/common")
+sys.path.append(r'//jpnas1a/Shared/SAG_PUBLIC/python/common')
+import common  # type: ignore # Assuming common has necessary functions including connect_yb
+from common import * # type: ignore
+
+data=dict(
+  common_criteria_tp = common.common_criteria('tp'),
+  common_chain_exclude_tp = common.common_chain_exclude('tp'),
+  common_exclude_ord_event_key_tbl = common.common_exclude_ord_event_key_tbl(),
+  common_event_dist_dst_pv = common.common_event_dist('dst', 'pv'),
+  common_event_dist_dst = common.common_event_dist('dst'),
+  common_id_filter_pos = common.common_id_filter('pos'),
+  common_id_filter_dst = common.common_id_filter('dst'),
+  common_id_filter_red = common.common_id_filter('red'),
+  common_event_ctrl_dst_pv = common.common_event_ctrl('dst', 'pv'),
+  common_event_red_red_pv = common.common_event_red('red', 'pv')
+)
+yaml_filepath='C:/Users/stewapatte/OneDrive - Catalina Marketing Japan K.K/ドキュメント/Code Chunks/RProfile/common_parameters.yml'
+with open(yaml_filepath, 'w') as outfile:
+  yaml.dump(data, outfile, default_flow_style=False)
